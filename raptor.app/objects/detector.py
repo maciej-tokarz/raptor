828ede151ec1cfbe26ff3178a9e64f04adc7aa3a
My-Poi! <maciej.tokarz@my-poi.pl>
2017-03-18 13:07:39 +0100

    Update detector.py

--- raptor.app/objects/detector.py
+++ raptor.app/objects/detector.py
@@ -1,7 +1,8 @@
 class Detector(object):
-    def __init__(self, gpio_id, status):
+    def __init__(self, gpio_id, status, camera):
         self._gpio_id = gpio_id
         self._status = status
+        self._camera = camera
 
 @property
 def gpio_id(self):
@@ -10,6 +11,10 @@ def gpio_id(self):
 @property
 def status(self):
     return self._status
+
+@property
+def camera(self):
+    return self._camera
 
 @status.setter
 def status(self, value):
