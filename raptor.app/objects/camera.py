8aea47860e7d8abfb5d485e3dcd836d29a3f666c
My-Poi! <maciej.tokarz@my-poi.pl>
2017-03-18 18:41:29 +0100

    Update camera.py

--- raptor.app/objects/camera.py
+++ raptor.app/objects/camera.py
@@ -1,21 +1,22 @@
 class Camera(object):
-    def __init__(self, id, resolution, detector):
+    def __init__(self, id, detector):
         self._id = id
-        self._resolution = resolution
+        self._resolution = (1920, 1080)
+        self._copyright = 'Raptor Cam {0}'.format(id)
         self._detector = detector
 
 @property
 def id(self):
     return self._id
 
-# Do przerobienia?
 def make_photo(self, path, file_name):
     try:
-        with PiCamera() as camera:
-            camera.resolution = (1920, 1080)
-            camera.exif_tags['IFD0.Copyright'] = 'Raptor'
-            time.sleep(1)
-            camera.capture('{0}{1}.jpg'.format(path, file_name))
+        if self._detector.status:
+            with PiCamera() as camera:
+                camera.resolution = self._resolution
+                camera.exif_tags['IFD0.Copyright'] = self._copyright
+                time.sleep(1)
+                camera.capture('{0}{1}.jpg'.format(path, file_name))
     except Exception as ex:
         self.logger.error('Camera: {0}'.format(ex))
         pass
